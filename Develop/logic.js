c911acccf26fe564cbaddd38cbebc94333dd2864
pmo48 <pmoriarty100@gmail.com>
2020-10-27 23:43:47 -0700

    added HTML

--- Develop/logic.js
+++ Develop/logic.js
@@ -10,7 +10,17 @@ console.log(now);
 //display current time
 $("#currentDay").text(now);
 
-// });
+//function to color time blocks
 
-})
+  //if time block is past the current time given time now, color the timeblock grey
+
+  //if time block is in the future, color the time block green
+
+  //if time block is where time is now, color the time block red
+
+//Function to save answers
+
+//Function to display answers
+
+});
 
